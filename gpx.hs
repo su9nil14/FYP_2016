e8e4e16a88a59b2b9c4b4f3da8f68ebe6ee4aa7f
sunil gautam <gautams@tcd.ie>
2016-02-22 13:30:24 +0000

    Added elevation component

--- gpx.hs
+++ gpx.hs
@@ -1,23 +1,25 @@
--- credit to : https://github.com/nurpax/gpx-utils
-{-# LANGUAGE Arrows, NoMonomorphismRestriction #-}
+{-# LANGUAGE Arrows #-}
 import Text.XML.HXT.Core
 import Data.Time(UTCTime, readTime, diffUTCTime)
 import Data.Time.Format(defaultTimeLocale)
 import Text.Printf (printf)
+import Text.Printf
 
 data Trkseg = Trkseg [Trkpt] deriving (Eq, Show)
 
+--define data structure
 data Trkpt = Trkpt {
   time :: UTCTime,
   longitude :: Double,
-  latitude :: Double
+  latitude :: Double,
+  elevation :: Double
   } deriving (Eq, Show)
 
--- Note: the hardcoded .000 part is kludge but for my inputs this was
--- an easy way to get timestamps to parse.
+-- derive time and timezone stuffs
 readt :: String -> UTCTime
-readt = readTime defaultTimeLocale "%FT%T.000%Z"
+readt = readTime defaultTimeLocale "%FT%T%Z"
 
+--do deep search on the gpx file
 atTag tag = deep (isElem >>> hasName tag)
 text = getChildren >>> getText
 
@@ -26,10 +28,12 @@ getTrkpt = atTag "trkpt" >>>
     time_ <- text <<< atTag "time" -< x
     lon <- getAttrValue "lon" -< x
     lat <- getAttrValue "lat" -< x
+    ele <- text <<< atTag "ele" -< x
     returnA -< Trkpt {
       time = readt time_,
       longitude = read lon,
-      latitude = read lat
+      latitude = read lat,
+      elevation = read ele
       }
 
 getTrkseg = atTag "trkseg" >>>
@@ -52,14 +56,20 @@ segmentLength pta ptb = r * c where
   a = (sin dlat/2)^2 + (sin dlon/2)^2 * cos lat1 * cos lat2
   c = 2 * atan2 (sqrt a) (sqrt (1-a))
 
+avgElev :: Trkpt -> Trkpt -> Double
+avgElev elev = e where
+  e = elevation
 -- Length of track as (seconds, kms)
-trackLength :: Trkseg -> (Double, Double)
-trackLength (Trkseg segs) = (timeLen, kmLen) where
+trackLength :: Trkseg -> (Double, Double, Double)
+trackLength (Trkseg segs) = (timeLen, kmLen, elevlen) where
     kmLen = sum (map (uncurry segmentLength) segments)
     timeLen = sum (map (uncurry timeDelta) segments)
+    elevlen = sum (map (uncurry avgElev) segments)
     segments = zip segs (tail segs)
     timeDelta :: Trkpt -> Trkpt -> Double
     timeDelta a b = realToFrac $ diffUTCTime (time b) (time a)
+    
+    
 
 formatTimeDeltaHMS :: Double -> String
 formatTimeDeltaHMS s =
@@ -71,11 +81,21 @@ formatTimeDeltaMS :: Double -> String
 formatTimeDeltaMS s =
   show (floor $ s / 60) ++ ":" ++ show (floor s `mod` 60)
 
+formatElevation:: Double -> String
+formatElevation s =
+  show (s)
+
+
+
+--TODO  REPL (Read-Evaluation-Print Loop)
+
 main :: IO ()
 main =
   do
-    trackSegs <- runX (readDocument [withValidate no] "test_files/activity1.gpx" >>> getTrkseg)
-    let (seconds, lenKm) = trackLength $ head trackSegs
+    trackSegs <- runX (readDocument [withValidate no] "test_files/test1.gpx" >>> getTrkseg)
+    let (seconds, lenKm, lenelev) = trackLength $ head trackSegs
     putStrLn (printf "Track distance (km):     %f" lenKm)
     putStrLn (printf "Track duration (h:m:s):  %s" $ formatTimeDeltaHMS seconds)
     putStrLn (printf "Average pace (min/km):   %s" $ formatTimeDeltaMS (seconds / lenKm))
+    putStrLn (printf "Above mean sea level (m):   %s" $ formatElevation (lenelev))
+
