16c58d82a4a0638cebdaf5cf8433e4a6fe281853
sunil gautam <gautams@tcd.ie>
2016-02-24 17:45:46 +0000

    few changes

--- gpxParser.hs
+++ gpxParser.hs
@@ -1,9 +1,17 @@
 {-# LANGUAGE Arrows #-}
+
+module Main (
+    main
+) where
+
 import Text.XML.HXT.Core
 import Data.Time(UTCTime, readTime, diffUTCTime)
 import Data.Time.Format(defaultTimeLocale)
 import Text.Printf (printf)
 import Text.Printf
+import System.Environment
+import Data.Tree.NTree.TypeDefs
+
 
 data Trkseg = Trkseg [Trkpt] deriving (Eq, Show)
 
@@ -20,7 +28,10 @@ readt :: String -> UTCTime
 readt = readTime defaultTimeLocale "%FT%T%Z"
 
 --do deep search on the gpx file
+atTag:: (ArrowXml a) => String -> a XmlTree XmlTree
 atTag tag = deep (isElem >>> hasName tag)
+
+text :: ArrowXml cat => cat (NTree XNode) String
 text = getChildren >>> getText
 
 getTrkpt = atTag "trkpt" >>>
@@ -55,10 +66,12 @@ segmentLength pta ptb = r * c where
   dlon = deg2rad $ longitude ptb - longitude pta
   a = (sin dlat/2)^2 + (sin dlon/2)^2 * cos lat1 * cos lat2
   c = 2 * atan2 (sqrt a) (sqrt (1-a))
-
+  
+--elevation
 avgElev :: Trkpt -> Trkpt -> Double
 avgElev elev = e where
   e = elevation
+
 -- Length of track as (seconds, kms)
 trackLength :: Trkseg -> (Double, Double, Double)
 trackLength (Trkseg segs) = (timeLen, kmLen, elevlen) where
@@ -84,18 +97,20 @@ formatTimeDeltaMS s =
 formatElevation:: Double -> String
 formatElevation s =
   show (s)
+  
 
-
+parseGPX :: String -> IOStateArrow s b XmlTree
+parseGPX file = readDocument [ withValidate yes, withRemoveWS yes] file
 
 --TODO  REPL (Read-Evaluation-Print Loop)
 
 main :: IO ()
 main =
   do
-    trackSegs <- runX (readDocument [withValidate no] "test_files/test1.gpx" >>> getTrkseg)
+    trackSegs <- runX (parseGPX "test_files/test1.gpx" >>> getTrkseg)
     let (seconds, lenKm, lenelev) = trackLength $ head trackSegs
     putStrLn (printf "Track distance (km):     %f" lenKm)
     putStrLn (printf "Track duration (h:m:s):  %s" $ formatTimeDeltaHMS seconds)
     putStrLn (printf "Average pace (min/km):   %s" $ formatTimeDeltaMS (seconds / lenKm))
-    putStrLn (printf "Above mean sea level (m):   %s" $ formatElevation (lenelev))
+    putStrLn (printf "Elevation (m):   %s" $ formatElevation (lenelev)) -- this gives wrong result
 
