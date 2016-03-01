0ee939d8cfc6f812e0d573f73111db6bdd10dcd9
sunil gautam <gautams@tcd.ie>
2016-03-01 14:26:45 +0000

    nothing much

--- gpxParser.hs
+++ gpxParser.hs
@@ -11,14 +11,15 @@ import Text.Printf (printf)
 import Text.Printf
 import System.Environment
 import Data.Tree.NTree.TypeDefs
-import Graphics.Gnuplot.Simple
+--import Graphics.Gnuplot.Simple
 import System.Console.Haskeline
 import Control.Monad.Trans
-
+import Data.Maybe
+import Control.Monad
+import Control.Arrow
 
 
 type Repl a = InputT IO a
-
 
 data Trkseg = Trkseg [Trkpt] deriving (Eq, Show)
 
@@ -85,17 +86,17 @@ timeDelta :: Trkpt -> Trkpt -> Double
 timeDelta a b = realToFrac $ diffUTCTime (time b) (time a)  
   
 --elevation
-avgElev :: Trkpt -> Trkpt -> Double
-avgElev elev = e where
-  e = elevation
+--getMaxElev :: Trkpt  -> Double
+--getMaxElev e= e where e = maximum[elevation]
+--minEle e = minimum elevation
 
   
 -- Length of track as (seconds, kms)
-trackLength :: Trkseg -> (Double, Double, Double)
-trackLength (Trkseg segs) = (timeLen, kmLen, elevlen) where
+trackLength :: Trkseg -> (Double, Double)
+trackLength (Trkseg segs) = (timeLen, kmLen) where
     kmLen = sum (map (uncurry segmentLength) segments)
     timeLen = sum (map (uncurry timeDelta) segments)
-    elevlen = sum (map (uncurry avgElev) segments)
+    --elevlen = map (avgElev)
     segments = zip segs (tail segs)
     
 
@@ -106,10 +107,12 @@ formatTimeDeltaHMS s =
   show (floor s `mod` 60)
 
   
-formatTimeDeltaMS :: Double -> String
-formatTimeDeltaMS s = show (s * 3.6) --convert mps to kmh
-  --show (floor $ s / 60) ++ ":" ++ show (floor s `mod` 60)
+formatTimeDeltaMPS2KMH :: Double -> String
+formatTimeDeltaMPS2KMH s = show (s * 3.6) --convert mps to kmh
 
+-- output as mins/km  
+formatTimeDeltaMS :: Double -> String
+formatTimeDeltaMS s = show (floor $ s / 60) ++ ":" ++ show (floor s `mod` 60)
   
 formatElevation:: Double -> String
 formatElevation s = show (s)
@@ -118,23 +121,34 @@ formatElevation s = show (s)
 parseGPX :: String -> IOStateArrow s b XmlTree
 parseGPX file = readDocument [ withValidate yes, withRemoveWS yes] file
 
-printOutput ::String ->IO ()
-printOutput file = do
+
+summarizeGPX ::String ->IO ()
+summarizeGPX file = do
  trackSegs <- runX (parseGPX file >>> getTrkseg)
- let (seconds, lenKm, lenelev) = trackLength $ head trackSegs
+ let (seconds, lenKm) = trackLength $ head trackSegs
  putStrLn (printf "Track distance (km):     %.2f" $ lenKm)
  putStrLn (printf "Track duration (h:m:s):  %s" $ formatTimeDeltaHMS seconds)
- putStrLn (printf "Average pace (km/hr):   %.4s" $ formatTimeDeltaMS (lenKm/seconds))
- putStrLn (printf "Elevation (m):   %s" $ formatElevation (lenelev)) -- this gives wrong result
+ putStrLn (printf "Average pace (km/hr):   %.4s" $ formatTimeDeltaMPS2KMH (lenKm/seconds))
+ putStrLn (printf "Average pace (mins/km):   %.4s" $ formatTimeDeltaMS (seconds/lenKm))
  putStrLn (printf "\n")
 
+ --repl setting
+mySettings :: Settings IO
+mySettings = Settings { historyFile = Just "myhist", complete = completeFilename, autoAddHistory = True}
+
+--todo
+help::String-> String
+help "load" = "Load a GPX file. Args = filename"
+help "help" = "Help... Add more later!"
+help "quit" = "Exit the program"
 
 repl :: Repl ()
 repl = do
-  minput <- getInputLine "Please enter input file - (path/filename) "
+  minput <- getInputLine "Please enter input file - (<path>/<filename>) :" 
   case minput of
     Nothing -> outputStrLn "Quitting."
-    Just input -> (liftIO $ printOutput input ) >> repl
+    Just  "quit"-> return()
+    Just input -> (liftIO $ summarizeGPX input ) >> repl
 
 main :: IO ()
-main = runInputT defaultSettings repl
+main = runInputT mySettings repl
